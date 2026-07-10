26c8a15fc091bf8201542d6ac840e85946aa373d
godaragourav963 <godaragourav963@gmail.com>
2026-07-10 09:05:24 +0530

    Initial commit

---
+++ basic/inputoutput.cpp
@@ -0,0 +1,9 @@
+#include<iostream>//we can use <bits/stdc++.h> this file include alomst everything so we dont have to add multiple and seperate folder for strings maths etc.
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;//to get input
+    cout << x;//to show output
+    return 0;
+}
---
+++ basic/practice.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int day;
+    cin >> day;
+
+    switch(day){
+        case 1:
+            cout <<"Monday";
+            break;
+        case 2:
+            cout <<"Tuesday";
+            break;
+        default:
+            cout << "Enter a valid number";
+            break;
+    }
+    return 0;
+}
